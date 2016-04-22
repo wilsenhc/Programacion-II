e077e6e6878c46464591badfe8240bdde51856a8
Wilsen Hernandez <wilsenh95@gmail.com>
2016-04-22 00:18:55 -0430

    Re-estrctura

--- EstructurasJerarquicas/Nodo.hpp
+++ EstructurasJerarquicas/Nodo.hpp
@@ -8,81 +8,22 @@ class NodoBT
 	private:
 		T key;
 		NodoBT<T> *parent, *left, *right;
+		
 	public:
-		NodoBT();
-		NodoBT(const T);
-		NodoBT(const NodoBT<T>&);
+		NodoBT() : key(0), parent(NULL), left(NULL), right(NULL) { };
+		NodoBT(const T in) : key(in), parent(NULL), left(NULL), right(NULL) { };
+		NodoBT(const NodoBT<T>& in) : key(in.key), parent(NULL), left(NULL), right(NULL) { };
 	
-		T getKey() const;
-		NodoBT<T>* getParent() const;
-		NodoBT<T>* getLeft() const;
-		NodoBT<T>* getRight() const;
+		T getKey() const { return key; }
+		NodoBT<T>* getParent() const { return parent; }
+		NodoBT<T>* getLeft() const { return left; }
+		NodoBT<T>* getRight() const { return right; }
 		
 		void setKey(const T);
 		void setParent(const NodoBT<T>*);
 		void setLeft(const NodoBT<T>*);
 		void setRight(const NodoBT<T>*);
 };
-
-/**
- * Constructor de Nodo (para Binary Tree)
- * @constructs NodoBT
- * */
-template<class T>
-NodoBT<T>::NodoBT()
-	: key( 0 ), parent( NULL ), left( NULL ), right( NULL ) { };
-	
-/**
- * Constructor de Nodo (para Binary Tree)
- * @constructs NodoBT
- * */
-template<class T>
-NodoBT<T>::NodoBT(const T in)
-	: key(in), parent( NULL ), left( NULL ), right( NULL ) { }
-
-/**
- * Constructor copia de Nodo (para Binary Tree)
- * @constructs NodoBT
- * */
-template<class T>
-NodoBT<T>::NodoBT(const NodoBT<T>& in)
-	: key(in.key), parent( NULL ), left( NULL ), right( NULL ) { }
-
-/**
- * Key getter
- * */
-template<class T>
-T NodoBT<T>::getKey() const
-{
-	return key;
-}
-
-/**
- * Parent Node getter
- * */
-template<class T>
-NodoBT<T>* NodoBT<T>::getParent() const
-{
-	return parent;
-}
-
-/**
- * Left Node getter
- * */
-template<class T>
-NodoBT<T>* NodoBT<T>::getLeft() const
-{
-	return left;
-}
-
-/**
- * Right Node getter
- * */
-template<class T>
-NodoBT<T>* NodoBT<T>::getRight() const
-{
-	return right;
-}
 
 /**
  * Key setter
