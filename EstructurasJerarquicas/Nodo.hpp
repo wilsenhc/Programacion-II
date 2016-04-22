8fbf3651f2561ce7d65c1578b38a158530b4e86f
Wilsen Hernandez <wilsenh95@gmail.com>
2016-04-21 22:53:40 -0430

    Nodo finalizado

--- EstructurasJerarquicas/Nodo.hpp
+++ EstructurasJerarquicas/Nodo.hpp
@@ -1,5 +1,6 @@
 #ifndef _NODO_BINARY_TREE_HPP_
 #define _NODO_BINARY_TREE_HPP_
+#include <iostream>
 
 template<class T>
 class NodoBT
@@ -9,17 +10,18 @@ class NodoBT
 		NodoBT<T> *parent, *left, *right;
 	public:
 		NodoBT();
+		NodoBT(const T);
 		NodoBT(const NodoBT<T>&);
 	
-		T getKey();
-		NodoBT<T>* getParent();
-		NodoBT<T>* getLeft();
-		NodoBT<T>* getRight();
+		T getKey() const;
+		NodoBT<T>* getParent() const;
+		NodoBT<T>* getLeft() const;
+		NodoBT<T>* getRight() const;
 		
-		void setKey(T);
-		void setParent(NodoBT<T>*);
-		void setLeft(NodoBT<T>*);
-		void setRight(NodoBT<T>*);
+		void setKey(const T);
+		void setParent(const NodoBT<T>*);
+		void setLeft(const NodoBT<T>*);
+		void setRight(const NodoBT<T>*);
 };
 
 /**
@@ -27,17 +29,95 @@ class NodoBT
  * @constructs NodoBT
  * */
 template<class T>
-NodoBT<T>::NodoBT<T>()
-	: key( 0 ), parent( NULL ), left( NULL ) right( NULL ) { };
+NodoBT<T>::NodoBT()
+	: key( 0 ), parent( NULL ), left( NULL ), right( NULL ) { };
+	
+/**
+ * Constructor de Nodo (para Binary Tree)
+ * @constructs NodoBT
+ * */
+template<class T>
+NodoBT<T>::NodoBT(const T in)
+	: key(in), parent( NULL ), left( NULL ), right( NULL ) { }
 
 /**
  * Constructor copia de Nodo (para Binary Tree)
  * @constructs NodoBT
  * */
 template<class T>
-NodoBT<T>::NodoBT<T>(const NodoBT<T>& in)
-	: key(in.key), parent( NULL ), left( NULL ) right( NULL ) { }
+NodoBT<T>::NodoBT(const NodoBT<T>& in)
+	: key(in.key), parent( NULL ), left( NULL ), right( NULL ) { }
 
+/**
+ * Key getter
+ * */
+template<class T>
+T NodoBT<T>::getKey() const
+{
+	return key;
+}
 
+/**
+ * Parent Node getter
+ * */
+template<class T>
+NodoBT<T>* NodoBT<T>::getParent() const
+{
+	return parent;
+}
+
+/**
+ * Left Node getter
+ * */
+template<class T>
+NodoBT<T>* NodoBT<T>::getLeft() const
+{
+	return left;
+}
+
+/**
+ * Right Node getter
+ * */
+template<class T>
+NodoBT<T>* NodoBT<T>::getRight() const
+{
+	return right;
+}
+
+/**
+ * Key setter
+ * */
+template<class T>
+void NodoBT<T>::setKey(const T item)
+{
+	key = item;
+}
+
+/**
+ * Parent setter
+ * */
+template<class T>
+void NodoBT<T>::setParent(const NodoBT<T>* p)
+{
+	parent = p;
+}
+
+/**
+ * Left setter
+ * */
+template<class T>
+void NodoBT<T>::setLeft(const NodoBT<T>* l)
+{
+	left = l;
+}
+
+/**
+ * Right setter
+ * */
+template<class T>
+void NodoBT<T>::setRight(const NodoBT<T>* r)
+{
+	right = r;
+}
 
 #endif
