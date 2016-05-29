cbdae048d8a1af0ab17b890a0fd9451c556c403b
Wilsen Hernandez <wilsenh95@gmail.com>
2016-05-29 14:39:22 -0400

    SintaxTree moved to main folder

---
+++ EstructurasJerarquicas/SintaxTree.hpp
@@ -0,0 +1,40 @@
+#ifndef _SINTAX_TREE_HPP_
+#define _SINTAX_TREE_HPP_
+#include <string>
+#include "../EstructurasLineales/Lista.hpp"
+#include "BinaryTree.hpp"
+
+using namespace std;
+
+class SintaxTree : public BinaryTree<string>
+{
+    public:
+		SintaxTree(Lista<string> p, Lista<string> in, Traverse t) : BinaryTree<string>(p, in, t) { };
+		
+        float evaluar() const;
+        
+    private:
+		float evaluar(NodeBT<string>*) const;
+};
+
+float SintaxTree::evaluar() const
+{
+	return evaluar(_root);
+}
+
+float SintaxTree::evaluar(NodeBT<string>* p) const
+{
+	if(p)
+	{
+		string key = p->getKey();
+	
+		if (key == "+") return evaluar (p->getLeft()) + evaluar(p->getRight());
+		else if (key == "-") return evaluar (p->getLeft()) - evaluar(p->getRight());
+		else if (key == "*") return evaluar (p->getLeft()) * evaluar(p->getRight());
+		else if (key == "/") return evaluar (p->getLeft()) / evaluar(p->getRight());
+		else return stof(key);
+	}
+	return 0;
+}
+
+#endif
