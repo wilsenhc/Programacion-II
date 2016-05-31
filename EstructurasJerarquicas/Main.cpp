f00af7be5f13263f8607b3c5428d547e91e141cc
Wilsen Hernandez <wilsenh95@gmail.com>
2016-05-31 04:55:44 -0400

    Testing BST

--- EstructurasJerarquicas/Main.cpp
+++ EstructurasJerarquicas/Main.cpp
@@ -1,71 +1,21 @@
 #include <iostream>
-
 #include "BinaryTree.hpp"
+#include "BST.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    Lista<int> pre, in, post;
+    BST<int> arbol;
     
-    pre.insertar(10,1);
-    pre.insertar(20,2);
-    pre.insertar(40,3);
-    pre.insertar(80,4);
-    pre.insertar(50,5);
-    pre.insertar(90,6);
-    pre.insertar(30,7);
-    pre.insertar(60,8);
-    pre.insertar(70,9);
-    pre.insertar(100,10);
-    pre.insertar(110,11);
-    pre.insertar(120,12);
+    arbol.insert(4);
+    arbol.insert(2);
+    arbol.insert(6);
+    arbol.insert(1);
+    arbol.insert(3);
+    arbol.insert(5);
     
-    in.insertar(80,1);
-    in.insertar(40,2);
-    in.insertar(20,3);
-    in.insertar(50,4);
-    in.insertar(90,5);
-    in.insertar(10,6);
-    in.insertar(60,7);
-    in.insertar(30,8);
-    in.insertar(100,9);
-    in.insertar(70,10);
-    in.insertar(110,11);
-    in.insertar(120,12);
-    
-    post.insertar(80,1);
-    post.insertar(40,2);
-    post.insertar(90,3);
-    post.insertar(50,4);
-    post.insertar(20,5);
-    post.insertar(60,6);
-    post.insertar(100,7);
-    post.insertar(120,8);
-    post.insertar(110,9);
-    post.insertar(70,10);
-    post.insertar(30,11);
-    post.insertar(10,12);
-    
-    BinaryTree<int> tree(pre, in, preorden);
-    cout << tree.isNull() << endl;
-    
-    BinaryTree<int> tree2;
-    
-    
-    cout << tree2.isNull() << endl;
-    
-    tree2 = tree.getRight();
-    cout << tree2.isNull() << endl;
-    
-    tree2.print(preorden);
-    
-    tree.destroy();
-    tree2.destroy();
-    
-    tree2.print(preorden);
-    cout << tree.isNull() << endl;
-    cout << tree2.isNull() << endl;
+    arbol.print(preorden);
     
     return 0;
 }
