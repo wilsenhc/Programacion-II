d4f555bfa72e01d75ed2bb3fea811db50d8c2c03
Wilsen Hernandez <wilsenh95@gmail.com>
2016-06-01 12:23:24 -0400

    Taller 2009-2010 resuelto

--- EstructurasJerarquicas/tests/taller2009/Main.cpp
+++ EstructurasJerarquicas/tests/taller2009/Main.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
+#include <string>
+#include "../../SintaxTree.hpp"
+#include "../../BinaryTree.hpp"
+#include "../../BST.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
+    Lista<string> porden, inorden;
+    SintaxTree *tree;
+    string s;
+    Traverse orden;
+    int i = 1;
+    
+    while (cin >> s)
+    {
+		if (i != 1)
+			cout << endl;
+		
+        if (s == "PREORDEN")
+			orden = preorden;
+		else if (s == "POSTORDEN")
+			orden = postorden;
+			
+        
+        while (cin.get() != '\n')
+        {
+            cin >> s;
+            porden.pushUltimo(s);
+        }
+        
+        cin >> s;
+        
+        while (cin.get() != '\n')
+        {
+            cin >> s;
+            inorden.pushUltimo(s);
+        }
+        
+        tree = new SintaxTree(porden, inorden, orden);
+        
+		cout << "Caso #" << i++ << ":" << endl 
+		<< tree->evaluar() << endl;
+		
+		porden.vaciar();
+		inorden.vaciar();
+		delete tree;
+		
+    }
     
     return 0;
 }
-
