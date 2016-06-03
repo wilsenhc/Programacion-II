d8e6eb6ce57c181de414c112936374dd2afa990d
Wilsen Hernandez <wilsenh95@gmail.com>
2016-06-02 21:10:44 -0400

    main

--- EstructurasJerarquicas/Main.cpp
+++ EstructurasJerarquicas/Main.cpp
@@ -8,6 +8,24 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    BST<string> tree;
     
+    tree.insert("S");
+    tree.insert("E");
+    tree.insert("X");
+    tree.insert("A");
+    tree.insert("R");
+    tree.insert("C");
+    tree.insert("H");
+    tree.insert("M");
+    tree.insert("W");
+    
+    cout << tree.min() << endl;
+    cout << tree.max() << endl;
+    
+    tree.del("S");
+    tree.del("E");
+    
+    return 0;
 }
 
