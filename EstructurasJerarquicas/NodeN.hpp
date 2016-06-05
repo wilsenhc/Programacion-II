02895917ec2c9fa455762b3ac17796ddff4de77f
Wilsen Hernandez <wilsenh95@gmail.com>
2016-06-05 01:27:01 -0400

    Constructor added, pointers' name changed

--- EstructurasJerarquicas/NodeN.hpp
+++ EstructurasJerarquicas/NodeN.hpp
@@ -1,24 +1,28 @@
 #ifndef _NODE_N_TREE_HPP_
 #define _NODE_N_TREE_HPP_
+#include <iostream>
 
 template<class T>
 class NodeN
 {
     private:
         T _key;
-        NodeN<T> *_leftChild, *_rightSilbing;
+        NodeN<T> *_left, *_right;
     
     public:
-        NodeN() : _leftChild(NULL), _rightSilbing(NULL) { };
-        NodeN(T e) : _key(e), _leftChild(NULL), _rightSilbing(NULL) { };
+        NodeN() : _left(NULL), _right(NULL) { };
+        NodeN(const NodeN<T>& e) : _key(e._key), _left(NULL), _right(NULL) { };
+        NodeN(T e) : _key(e), _left(NULL), _right(NULL) { };
+        NodeN(T e, NodeN<T> *c) : _key(e), _left(c), _right(NULL) { };
+        NodeN(T e, NodeN<T>* a, NodeN<T>* b) : _key(e), _left(a), _right(b) { };
         
         T getKey() const { return _key; }
-        NodeN<T>* getLeft() const { return _leftChild; }
-        NodeN<T>* getRight() const { return _rightSilbing; }
+        NodeN<T>* getLeft() const { return _left; }
+        NodeN<T>* getRight() const { return _right; }
         
         void setKey(const T);
-        void setLeft(const NodeN<T>*);
-        void setRight(const NodeN<T>*);
+        void setLeft(NodeN<T>*);
+        void setRight(NodeN<T>*);
 };
 
 /**
@@ -34,18 +38,18 @@ void NodeN<T>::setKey(const T item)
  * Left child setter.
  * */
 template<class T>
-void NodeN<T>::setLeft(const NodeN<T>* l)
+void NodeN<T>::setLeft(NodeN<T>* l)
 {
-    _leftChild = l;
+    _left = l;
 }
 
 /**
  * Right Silbing setter.
  * */
 template<class T>
-void NodeN<T>::setRight(const NodeN<T>* r)
+void NodeN<T>::setRight(NodeN<T>* r)
 {
-    _rightSilbing = r;
+    _right = r;
 }
 
 #endif
