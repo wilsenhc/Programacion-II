2bb972b8dceb2d5ec4f75d9f37d0788619645250
Wilsen Hernandez <wilsenh95@gmail.com>
2016-07-05 16:24:28 -0400

    Insert Arc

--- EstructurasMultienlazadas/Vertex.hpp
+++ EstructurasMultienlazadas/Vertex.hpp
@@ -4,52 +4,66 @@
 #include "Arc.hpp"
 
 template<class T, class C>
+class Graph;
+
+template<class T, class C>
 class Arc;
 
 template<class T, class C>
 class Vertex
 {
     private:
+        friend class Graph<T,C>;
         T key;
-        Arc<T,C> *ady;
-        Vertex<T,C> *next;
+        Arc<T,C>* ady;
+        Vertex* next;
 
     public:
         Vertex() : ady(NULL), next(NULL) { };
         Vertex(T info) : key(info), ady(NULL), next(NULL) { };
+        ~Vertex();
 
         T getKey() const { return key; };
-        void insertArc(Vertex<T,C>*, C);
+        Vertex* getNext() const { return next; };
+
+        void setKey(T k) { key = k; };
+        void setNext(Vertex* p) { next = p; };
+
+        void insertArc(Vertex*, C);
+        
 };
 
 template<class T, class C>
-void Vertex<T,C>::insertArc(Vertex<T,C> *w, C cost)
+void Vertex<T,C>::insertArc(Vertex *w, C cost)
 {
-    Arc<T,C> *pivot = this->ady;
-    Arc<T,C> *pivotL = NULL;
-    bool connected = false;
-
-    while (pivot != NULL && !connected)
+    if (this != w)
     {
-        connected = (pivot->getKey() == w->getKey());
-        pivotL = pivot;
-        pivot = pivot->getNext();
-    }
+        Arc<T,C> *pivot = this->ady;
+        Arc<T,C> *pivotL = NULL;
+        bool connected = false;
 
-    if (!connected)
-    {
-        if (!pivotL && !pivot)
+        while (pivot != NULL && !connected)
         {
-            pivot = this->ady;
-            Arc<T,C> *v;
-            v = new Arc<T,C>();
-            v->setCost(cost);
-            v->setVertex(w);
-            v->setNext(pivot);
-            this->ady = v;
+            connected = (pivot->getKey() == w->getKey());
+            pivotL = pivot;
+            pivot = pivot->getNext();
         }
-        else
-            pivotL->setNext(new Arc<T,C>(cost, w, NULL));
+
+        if (!connected)
+        {
+            if (!pivotL && !pivot)
+            {
+                pivot = this->ady;
+                Arc<T,C> *v;
+                v = new Arc<T,C>();
+                v->setCost(cost);
+                v->setVertex(w);
+                v->setNext(pivot);
+                this->ady = v;
+            }
+            else
+                pivotL->setNext(new Arc<T,C>(cost, w, NULL));
+        }           
     }
 }
 
