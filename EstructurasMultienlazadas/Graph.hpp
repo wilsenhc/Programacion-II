35472661f2545133fe7e27ca9509c803e598a5ca
Wilsen Hernandez <wilsenh95@gmail.com>
2016-07-05 16:20:21 -0400

    Copy constructor

--- EstructurasMultienlazadas/Graph.hpp
+++ EstructurasMultienlazadas/Graph.hpp
@@ -11,6 +11,37 @@ class Graph
 
     public:
         Graph() : graph(NULL) { };
+        Graph(const Graph &g);
 };
+
+template<class T, class C>
+Graph<T,C>::Graph(const Graph &g)
+{
+    if (g.graph != NULL)
+    {
+        Vertex<T,C> *p;
+        Vertex<T,C> *q;
+        this->graph = new Vertex<T,C>(g.graph->getKey());
+        p = g.graph;
+        q = this->graph;
+
+        while (p->getNext() != NULL)
+        {
+            p = p->getNext();
+            q->setNext(new Vertex<T,C>(p->getKey()));            
+            q = q->getNext();
+        }
+
+        for (p = g.graph; p; p = p->getNext())
+        {
+            Arc<T,C> *a = p->ady;
+            while (a)
+            {
+                this->insertArc(p->getKey(), a->getKey(), a->getCost());
+                a = a->getNext();
+            }
+        }
+    }
+}
 
 #endif
