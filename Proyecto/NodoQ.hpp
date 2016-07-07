5bd0c84d30acf4672d6d0d6edc19d8932d252d4c
Wilsen Hernandez <wilsenh95@gmail.com>
2016-07-07 13:53:02 -0400

    PROYECTO NodoQ

--- Proyecto/NodoQ.hpp
+++ Proyecto/NodoQ.hpp
@@ -2,19 +2,23 @@
 #define _NODO_QUAD_TREE_HPP_
 #include <iostream>
 
-template<class T>
+typedef enum Color { BLANCO, NEGRO, GRIS };
+
 class NodoQ
 {
     private:
         static const int MAX = 4;
-        T info;
+        Color info;
         NodoQ* hijos[MAX];
 
     public:
-        NodoQ() : hijos({NULL, NULL, NULL, NULL}) {};
-        NodoQ(T e) : info(e), hijos({NULL, NULL, NULL, NULL}) {};
+        NodoQ() { for(int i = 0; i < MAX; i++) hijos[i] = NULL; };
+        NodoQ(Color e) : info(e) { for(int i = 0; i < MAX; i++) hijos[i] = NULL; };
         
-        T obtInfo() const { return info; };
+        Color obtInfo() const { return info; };
+        NodoQ* obtHijos() const { return hijos; };
+
+        void setInfo(Color e) { info = e; };
         
 
 };
