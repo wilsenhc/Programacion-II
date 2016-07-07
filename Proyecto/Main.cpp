6a599c1ebe3ef2df93dd84f8b32462fd3036ecc7
Wilsen Hernandez <wilsenh95@gmail.com>
2016-07-07 13:53:13 -0400

    TEST MAIN PROYECTO

---
+++ Proyecto/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+//~ #include "QuadTree.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        char key;
+        list<char> cadena1, cadena2;
+        do
+        {
+            cin >> key;
+            cout << key;
+        }while (cin);
+        
+        //~ list<char>::iterator j;
+        //~ for (j = cadena1.begin(); j != cadena1.end(); ++j)
+            //~ cout << *j << " ";
+    }
+
+    return 0;
+}
