f4e5b5b8c4400da1bc1ff936cdd08998a4a32087
Wilsen Hernandez <wilsenh95@gmail.com>
2016-07-07 23:30:28 -0400

    Test main

--- EstructurasMultienlazadas/Main.cpp
+++ EstructurasMultienlazadas/Main.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include "Graph.hpp"
 
 using namespace std;
 
-void a(Graph<int,int> valor, Graph<int,int> referencia)
-{
-    int a = 10;
-    a *= 10;
-}
-
 int main(int argc, char **argv)
 {
-    Graph<int,int> G;
+    Graph<int,int> G, F;
+    vector<int> b;
     
     srand(time(NULL));
-    for (int i = 0; i < 10; i++)
-        G.insertVertex(abs(rand() % 10));
+    for (int i = 0; i < 15; i++)
+        G.insertVertex(abs(rand() % 10) + 1);
         
     for (int i = 0; i < 40; i++)
-        G.insertArc(abs(rand() % 10), abs(rand() % 10), 0);
+        G.insertArc(abs(rand() % 10) + 1, abs(rand() % 10) + 1, rand() % 10);
     
-    a(G, G);
+    cout << G << endl;
     
-    int a;
-    cin >> a;
-    G.deleteVertex(a);
+    b = G.sourceVertices();
+    cout << "Source vertices: ";
+    for (int i : b)
+        cout << i << " ";
+        
+    b = G.sinkVertices();
+    cout << endl << "Sink vertices: ";
+    for (int i : b)
+        cout << i << " ";
     
+    cout << endl << "Orden: " << G.order() << endl;
+    int n, m;
+    
+    cout << "BFS: ";
+    b = G.breadthFirstSearch();
+    for (int i : b)
+        cout << i << " ";
+    cout << endl;
+    
+    cin >> n;
+    cout << "BFS (" << n << "): ";
+    b = G.breadthFirstSearch(n);
+    for (int i : b)
+        cout << i << " ";
+    cout << endl;
+    
+    cout << "DFS: ";
+    b = G.depthFirstSearch();
+    for (int i : b)
+        cout << i << " ";
+    cout << endl;
+       
     return 0;
 }
